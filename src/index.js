616f98a43051ffd5b0ddcf83aded45e1b911ad66
Jan Armbrust <jan.armbrust@gmx.de>
2017-07-01 20:42:29 +0200

    Added english pound, refactored code and prevented skill from returning errors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ var APP_ID = undefined;
 var SKILL_NAME = "Krypto Kurs";
 var GET_FACT_MESSAGE = "Hier ist dein zufälliger Kurs: ";
 var HELP_MESSAGE = "Du kannst mich nach einem Kurs für eine beliebige Kryptowährung fragen oder eine bestimmte Anzahl einer Kryptowährung in eine konventionelle Währung umrechnen lassen.";
-var HELP_REPROMPT = "Womit kann ich dir helfen?";
 var STOP_MESSAGE = "Auf Wiedersehen und danke, dass du Krypto Kurs benutzt hast!";
 
 var data = [
@@ -35,11 +34,8 @@ var data = [
   "Dogecoin",
   "Lisk",
   "Byteball"
-]
+];
 
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
@@ -69,7 +65,8 @@ var handlers = {
                     emitString = 'Ein ' + myResult[0].name + ' ist momentan ' + numberCurrencyValue + ' € wert';
                 }
                 var speechOutput = GET_FACT_MESSAGE + emitString;
-                this.emit(':tellWithCard', speechOutput, SKILL_NAME, emitString);
+                var smallImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/5/50/Bitcoin.png';
+                this.emit(':tellWithCard', speechOutput, SKILL_NAME, emitString, smallImageUrl);
         });
     },
     'GetCryptoCurrencyExchangeRateInCurrencyIntent': function () {
@@ -79,9 +76,12 @@ var handlers = {
 
         if (currency == 'euro') {
             currencyShort = 'EUR';
-        }
-        if ((currency == 'dollar') || (currency == 'us-dollar')) {
+        } else if (currency == 'dollar') {
             currencyShort = 'USD';
+        } else if (currency == 'pfund') {
+            currencyShort = 'GBP';
+        } else {
+            currencyShort = 'EUR';
         }
 
         var myRequest = {crCu:cryptoCurrency,cuSh:currencyShort};
@@ -90,69 +90,101 @@ var handlers = {
                 this.attributes.lastSearch.lastSpeech = myResult;
                 var emitString = '';
                 var numberCurrencyValue = 0.0;
-                if (currencyShort == 'USD') {
-                    numberCurrencyValue = Math.floor((Number(myResult[0].price_usd)) * 100) / 100;
-                    emitString = 'Ein ' + myResult[0].name + ' ist momentan $ ' + numberCurrencyValue + ' wert';
+                if (myResult[0] === undefined) {
+                    var speechOutput = 'Das habe ich leider nicht verstanden, bitte versuche es noch einmal.';
+                    this.emit(':ask', speechOutput);
+                } else {
+                    if (currencyShort == 'USD') {
+                        numberCurrencyValue = Math.floor((Number(myResult[0].price_usd)) * 100) / 100;
+                        emitString = 'Ein ' + myResult[0].name + ' ist momentan $ ' + numberCurrencyValue + ' wert';
+                    }
+                    if (currencyShort == 'EUR') {
+                        numberCurrencyValue = Math.floor((Number(myResult[0].price_eur)) * 100) / 100;
+                        emitString = 'Ein ' + myResult[0].name + ' ist momentan ' + numberCurrencyValue + ' € wert';
+                    }
+                    if (currencyShort == 'GBP') {
+                        numberCurrencyValue = Math.floor((Number(myResult[0].price_gbp)) * 100) / 100;
+                        emitString = 'Ein ' + myResult[0].name + ' ist momentan £ ' + numberCurrencyValue + ' wert';
+                    }
+                    this.emit(':tell', emitString);
                 }
-                if (currencyShort == 'EUR') {
-                    numberCurrencyValue = Math.floor((Number(myResult[0].price_eur)) * 100) / 100;
-                    emitString = 'Ein ' + myResult[0].name + ' ist momentan ' + numberCurrencyValue + ' € wert';
-                }
-                this.emit(':tell', emitString );
+
         });
     },
     'GetAmountOfCryptoCurrencyInCurrencyIntent': function () {
       var amount = this.event.request.intent.slots.amount.value;
       amount = parseInt(amount);
-      var currency = this.event.request.intent.slots.currency.value;
-      var cryptoCurrency = this.event.request.intent.slots.cryptoCurrency.value;
-      var currencyShort = '';
-      console.log('Wert von amount: ', amount);
-      console.log('Wert von currency: ', currency);
-      console.log('Wert von cryptoCurrency: ', cryptoCurrency);
+      if (isNaN(amount)) {
+        var speechOutput = 'Das habe ich leider nicht verstanden, bitte versuche es noch einmal.';
+        this.emit(':ask', speechOutput);
+      } else {
+          var currency = this.event.request.intent.slots.currency.value;
+          var cryptoCurrency = this.event.request.intent.slots.cryptoCurrency.value;
+          var currencyShort = '';
+          console.log('Wert von amount: ', amount);
+          console.log('Wert von currency: ', currency);
+          console.log('Wert von cryptoCurrency: ', cryptoCurrency);
 
-      if (currency == 'euro') {
-          currencyShort = 'EUR';
-      }
-      if ((currency == 'dollar') || (currency == 'us-dollar')) {
-          currencyShort = 'USD';
-      }
-      console.log('Wert von currencyShort: ', currencyShort);
+          if (currency == 'euro') {
+              currencyShort = 'EUR';
+          } else if (currency == 'dollar') {
+              currencyShort = 'USD';
+          } else if (currency == 'pfund') {
+              currencyShort = 'GBP';
+          } else {
+              currencyShort = 'EUR';
+          }
+          console.log('Wert von currencyShort: ', currencyShort);
 
-      var myRequest = {crCu:cryptoCurrency,cuSh:currencyShort};
-      httpGet(myRequest,  (myResult) => {
-              this.attributes.lastSearch = {currency};
-              this.attributes.lastSearch.lastSpeech = myResult;
-              var emitString = '';
-              var numberCurrencyValue = 0.0;
-              var amountOfCryptoCurrencyInCurrency = 0.0;
-              if (currencyShort == 'USD') {
-                  numberCurrencyValue = Math.floor((Number(myResult[0].price_usd)) * 100) / 100;
-                  amountOfCryptoCurrencyInCurrency = amount * numberCurrencyValue;
-                  amountOfCryptoCurrencyInCurrency = Math.floor((Number(amountOfCryptoCurrencyInCurrency)) * 100) / 100;
-                  if (amount > 1) {
-                    emitString = amount + ' ' + myResult[0].name + ' sind momentan $ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+          var myRequest = {crCu:cryptoCurrency,cuSh:currencyShort};
+          httpGet(myRequest,  (myResult) => {
+                  this.attributes.lastSearch = {currency};
+                  this.attributes.lastSearch.lastSpeech = myResult;
+                  var emitString = '';
+                  var numberCurrencyValue = 0.0;
+                  var amountOfCryptoCurrencyInCurrency = 0.0;
+                  if (myResult[0] === undefined) {
+                      var speechOutput = 'Das habe ich leider nicht verstanden, bitte versuche es noch einmal.';
+                      this.emit(':ask', speechOutput);
                   } else {
-                    emitString = amount + ' ' + myResult[0].name + ' ist momentan $ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+                      if (currencyShort == 'USD') {
+                          numberCurrencyValue = Math.floor((Number(myResult[0].price_usd)) * 100) / 100;
+                          amountOfCryptoCurrencyInCurrency = amount * numberCurrencyValue;
+                          amountOfCryptoCurrencyInCurrency = Math.floor((Number(amountOfCryptoCurrencyInCurrency)) * 100) / 100;
+                          if (amount > 1) {
+                            emitString = amount + ' ' + myResult[0].name + ' sind momentan $ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+                          } else {
+                            emitString = amount + ' ' + myResult[0].name + ' ist momentan $ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+                          }
+                      }
+                      if (currencyShort == 'EUR') {
+                          numberCurrencyValue = Math.floor((Number(myResult[0].price_eur)) * 100) / 100;
+                          amountOfCryptoCurrencyInCurrency = amount * numberCurrencyValue;
+                          amountOfCryptoCurrencyInCurrency = Math.floor((Number(amountOfCryptoCurrencyInCurrency)) * 100) / 100;
+                          if (amount > 1) {
+                            emitString = amount + ' ' + myResult[0].name + ' sind momentan ' + amountOfCryptoCurrencyInCurrency + ' € wert';
+                          } else {
+                            emitString = amount + ' ' + myResult[0].name + ' ist momentan ' + amountOfCryptoCurrencyInCurrency + ' € wert';
+                          }
+                      }
+                      if (currencyShort == 'GBP') {
+                          numberCurrencyValue = Math.floor((Number(myResult[0].price_gbp)) * 100) / 100;
+                          amountOfCryptoCurrencyInCurrency = amount * numberCurrencyValue;
+                          amountOfCryptoCurrencyInCurrency = Math.floor((Number(amountOfCryptoCurrencyInCurrency)) * 100) / 100;
+                          if (amount > 1) {
+                            emitString = amount + ' ' + myResult[0].name + ' sind momentan £ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+                          } else {
+                            emitString = amount + ' ' + myResult[0].name + ' ist momentan £ ' + amountOfCryptoCurrencyInCurrency + ' wert';
+                          }
+                      }
+                      this.emit(':tell', emitString );
                   }
-              }
-              if (currencyShort == 'EUR') {
-                  numberCurrencyValue = Math.floor((Number(myResult[0].price_eur)) * 100) / 100;
-                  amountOfCryptoCurrencyInCurrency = amount * numberCurrencyValue;
-                  amountOfCryptoCurrencyInCurrency = Math.floor((Number(amountOfCryptoCurrencyInCurrency)) * 100) / 100;
-                  if (amount > 1) {
-                    emitString = amount + ' ' + myResult[0].name + ' sind momentan ' + amountOfCryptoCurrencyInCurrency + ' € wert';
-                  } else {
-                    emitString = amount + ' ' + myResult[0].name + ' ist momentan ' + amountOfCryptoCurrencyInCurrency + ' € wert';
-                  }
-              }
-              this.emit(':tell', emitString );
-          });
+              });
+          }
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = HELP_MESSAGE;
-        var reprompt = HELP_REPROMPT;
-        this.emit(':ask', speechOutput, reprompt);
+        this.emit(':tell', speechOutput);
     },
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', STOP_MESSAGE);
@@ -194,10 +226,10 @@ function httpGet(myData, callback) {
             // We can see it in the log output via:
             // console.log(JSON.stringify(returnData))
             // we may need to parse through it to extract the needed data
-            console.log('Zurückgegebene Daten: ', returnData, typeof(returnData));
+            //console.log('Zurückgegebene Daten: ', returnData, typeof(returnData));
             var returnDataJSON = JSON.parse(returnData);
-            console.log('Zurückgegebener Datenstring als Objekt: ', returnDataJSON);
-            console.log('Zurückgegebener Datenstring als Objekt, zugriff auf Key name: ', returnDataJSON[0].price_eur);
+            //console.log('Zurückgegebener Datenstring als Objekt: ', returnDataJSON);
+            //console.log('Zurückgegebener Datenstring als Objekt, zugriff auf Key name: ', returnDataJSON[0].price_eur);
             callback(returnDataJSON);  // this will execute whatever function the caller defined, with one argument
         });
 
